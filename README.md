d33f05b16f8e995df98bf191e9a1078d74cad708
Juliana Krueger <jrk16e@my.fsu.edu>
2018-10-19 21:07:32 +0000

    Lesson 4 final commit

--- README.md
+++ README.md
@@ -13,12 +13,11 @@ Web Application Development
 (This assignment was a combination of Cloud 9 and Git Hub. The two together allowed me to create the page for this assignment and plug in the correct data.)
 
 ### View Project:
-(Replace this statement with your GitHub Page URL that was created when you 
- published the project.)
+( https://jrk16e.github.io/lesson4_javascript1/ )
 
 ### Lessons Learned in the Assignment:
 1. (A concept I learned in this lesson is how to use Git Hub better.)
-2. (Learned how to enter codes on the terminal in Cloud 9.)
+2. (Learned how to enter codes on the terminal in Cloud 9 and how important it is to have the right information.)
 3. (Learned that it can take multiple tries to push the files from Git Hub using HTML or SSH.)
 
 
--- expenseCalculator.js
+++ expenseCalculator.js
@@ -147,7 +147,7 @@
                   document.getElementById("totalExpenses").innerHTML = "<b>Total Expenses:     $</b> " + totalExpenses.toFixed(2);
                   document.getElementById("shelterPercentage").innerHTML = "% for <b>shelter</b>:      " + shelterPct.toFixed(1);      
                   document.getElementById("foodPercentage").innerHTML = "% for <b>food:</b>      " + foodPct.toFixed(1);
-                  document.getElementById("householdPercentage").innerHTML = "% for <b>household:</b>      " + /*globalhouseholdPct.toFixed(1); 
+                  document.getElementById("householdPercentage").innerHTML = "% for <b>household:</b>      " + /*householdPct.toFixed(1); 
                   document.getElementById("transportationPercentage").innerHTML = "% for <b>transportation:</b>      " + transportationPct.toFixed(1);
                   document.getElementById("miscellaneousPercentage").innerHTML = "% for <b>miscellaneous:</b>      " + miscellaneousPct.toFixed(1);
                   
